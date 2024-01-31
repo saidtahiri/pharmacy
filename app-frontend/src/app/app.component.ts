3f96661e4e2222e5ff7c5ffd71d6df0bbd3b15c6
Tahiri Said <said.tahiri@hotmail.com>
2024-01-31 15:18:47 +0100

    changes

--- app-frontend/src/app/app.component.ts
+++ app-frontend/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
 import { NavbarComponent } from "./component/navbar/navbar.component";
 import { FooterComponent } from "./component/footer/footer.component";
-import { LeftasideallcategoriesComponent } from './component/LeftAsideAllCategories/leftasideallcategories.component';
+
 import { SearchallcategoriesComponent } from './component/searchallcategories/searchallcategories.component';
 import { TopHeaderComponent } from './component/navbar/top-header/top-header.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -18,7 +18,7 @@ import { SignupComponent } from './pages/signup/signup.component';
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     imports: [RouterLink, CommonModule, RouterOutlet, HomeComponent, HomeComponent, NavbarComponent, FooterComponent,
-    LeftasideallcategoriesComponent,SearchallcategoriesComponent,TopHeaderComponent,LoginComponent]
+    SearchallcategoriesComponent,TopHeaderComponent,LoginComponent]
 })
 export class AppComponent {
   title = 'app-frontend';
