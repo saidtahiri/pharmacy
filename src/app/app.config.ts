a964067a078d61aadfdbe4cb895181562fd0f99a
Tahiri Said <said.tahiri@hotmail.com>
2024-02-05 14:37:22 +0100

    Updates

--- src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { ProductService } from './services/products/product.service';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
-};
